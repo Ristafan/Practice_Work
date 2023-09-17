ecc2ee5f59455483912f6fda55b3ade594aab661
Ristafan <121894103+Ristafan@users.noreply.github.com>
2023-09-17 16:36:48 +0200

    added LinSearch1 & 2

--- Linear_Search(C).c
+++ Linear_Search(C).c
@@ -2,7 +2,29 @@
 #include <stdlib.h>
 #include <math.h>
 
+int LinSearch1(int arr[], int len, int obj){
+    int a = -1;
+    for (int i = 0; i < len; i++){
+        if (arr[i] == obj){
+            a = i;
+        };
+    };
 
+    return a;
+};
+
+int LinSearch2(int arr[], int len, int obj){
+    int a = -1;
+    int i = 0;
+    while (i < len){
+        if (arr[i] == obj){
+            a = i;
+            break;
+        };
+    };
+
+    return a;
+}
 
 int main(){
 
