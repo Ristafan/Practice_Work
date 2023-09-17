0a693af65824a791bc4ed8b20fb466da2bdc6793
Ristafan <121894103+Ristafan@users.noreply.github.com>
2023-09-17 16:45:36 +0200

    added print statement

--- Linear_Search(C).c
+++ Linear_Search(C).c
@@ -28,8 +28,14 @@ int LinSearch2(int arr[], int len, int obj){
 
 int main(){
 
+    int list[6] = {3, 4, 1, 8, 2, 4};
+    int len = 6;
+    int nr = 4;
 
+    int a = LinSearch1(list, len, nr);
+    int b = LinSearch2(list, len, nr);
 
+    printf("For LinSearch1 the nr. 4 is at pos %i in the array and at pos %i in LinSearch2", a, b);
 
     return 0;
 }
