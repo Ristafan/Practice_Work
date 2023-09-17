68fa3c10123e79ffb92d2fcba77925c73f8cf10c
ristafan <martin.faehnrich@gmail.com>
2023-09-17 16:09:01 +0200

    added LinSearch1

--- Linear_Search(Python).py
+++ Linear_Search(Python).py
@@ -1,3 +1,11 @@
 #Linear Search
 
 List = [3, 4, 1, 8, 2, 4]
+
+def LinSearch1(list, obj):
+    a = -1
+    for i in range(len(list)):
+        if list[i] == obj:
+            a = i
+    
+    return a
