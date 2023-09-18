4f83a3e656012fdb18e57486dcc6f5a042331202
Ristafan <121894103+Ristafan@users.noreply.github.com>
2023-09-18 21:18:04 +0200

    minor changes

---
+++ BinSearch(Python).py
@@ -0,0 +1,22 @@
+#BinSearch
+
+Array = [1, 3, 5, 7, 9]
+
+def BinSearch(list, nmb):
+    r = len(list)-1
+    l = 0
+    m = int((l+r)/2)
+
+    while list[m] != nmb and l < r:
+        if list[m] < nmb:
+            l = m+1
+        if list[m] > nmb:
+            r = m
+
+    if list[m] == nmb:
+        return m
+    else: return -1
+
+
+a = BinSearch(Array, 3)
+print("The number you were looking for is in space %i", a)
